b32ccc774b4fb5a2375d0a4f3eb66213396c7e8c
Mikk155 <martimsanchez155@gmail.com>
2024-11-01 16:54:06 -0300

    Update env.py

--- .github/workflows/env.py
+++ .github/workflows/env.py
@@ -1,7 +1,7 @@
-class CEnv(dict):
+class CEnv:
     def __init__( self ):
-        super().__init__( {} );
+        super().__init__();
     def __getattr__( self, key ) -> str:
         from os import getenv;
-        return str( getenv( key ) ) if getenv( key ) in self else '';
+        return str( getenv( key ) ) if getenv( key ) else '';
 env: CEnv = CEnv();
