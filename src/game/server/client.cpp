39a7bf26233ee031abc15be3b93832cbd983c148
Mikk155 <martimsanchez155@gmail.com>
2024-11-05 09:43:17 -0300

    ClientCommand for swaping inventory items

--- src/game/server/client.cpp
+++ src/game/server/client.cpp
@@ -617,14 +617,18 @@ void SV_CreateClientCommands()
 			} },
 		{.Flags = ClientCommandFlag::Cheat});
 
-	g_ClientCommands.Create("use", [](CBasePlayer* player, const auto& args)
-		{ player->SelectItem(args.Argument(1)); });
-
 	g_ClientCommands.Create("slot", [](CBasePlayer* player, const auto& args)
 	{
 		if( args.Count() > 1 )
 		{
 			player->inventory_active_item = std::clamp( atoi( args.Argument(1) ), minecraft::slot::SLOT1, minecraft::slot::SLOT9 );
+	} } );
+
+	g_ClientCommands.Create("swap", [](CBasePlayer* player, const auto& args)
+	{
+		if( args.Count() > 2 )
+		{
+			std::swap( player->inventory[atoi(args.Argument(1))], player->inventory[atoi(args.Argument(2))] );
 	} } );
 
 	g_ClientCommands.Create("gamemode", [](CBasePlayer* player, const CommandArgs& args)
