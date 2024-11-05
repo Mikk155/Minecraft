824f5f293054239450ac9f332612fdfa74c158a5
Gaftherman <fano.solis.campos@gmail.com>
2024-11-04 23:35:58 -0500

    wea csmr pq al precionar click derecho se desactiva xd

--- src/game/client/ui/hud/inventory.cpp
+++ src/game/client/ui/hud/inventory.cpp
@@ -21,7 +21,7 @@
 #include "vgui_TeamFortressViewport.h"
 
 void IN_ResetMouse();
-extern bool g_iVisibleMouse;
+extern bool g_iVisibleMouse, iMouseInUse;
 
 bool CHudInventory::Init()
 {
@@ -42,35 +42,49 @@ void CHudInventory::Reset()
 
 bool CHudInventory::VidInit()
 {
-	m_HUD_inventory = gHUD.GetSpriteIndex("inventory");
+	if (ScreenWidth < 640)
+		m_HUD_inventory = gHUD.GetSpriteIndex("320inventory");
+	else
+		m_HUD_inventory = gHUD.GetSpriteIndex("640inventory");
 
 	return true;
 }
 
 bool CHudInventory::Draw(float flTime)
 {
+	ConsolePrint(std::string("m_fOn: " + std::to_string(m_fOn) + "\n").c_str());
+	ConsolePrint(std::string("g_iVisibleMouse: " + std::to_string(g_iVisibleMouse) + "\n").c_str());
+	ConsolePrint(std::string("iMouseInUse: " + std::to_string(iMouseInUse) + "\n").c_str());
+
 	// don't draw the menu if the scoreboard is being shown
 	if (gHUD.m_Scoreboard.m_iShowscoresHeld)
 		return true;
+
+	if (m_fOn && !g_iVisibleMouse)
+	{
+		g_iVisibleMouse = true;
+		App::getInstance()->setCursorOveride(App::getInstance()->getScheme()->getCursor(Scheme::scu_crosshair));
+	}
 
 	// No esta activado = no dibujar en pantalla
 	if (!m_fOn)
 		return true;
 
-	//gEngfuncs.pfnSPR_Set(m_hudSprite, 255, 255, 255);
 	SPR_Set(gHUD.GetSprite(m_HUD_inventory), RGB_WHITE);
-
 	Rect rect = gHUD.GetSpriteRect(m_HUD_inventory);
-	//gEngfuncs.pfnSPR_DrawAdditive(0, (ScreenWidth / 2) - width / 2, (ScreenHeight / 2) - height / 2, &drawingRect);
 	SPR_Draw(0, (ScreenWidth / 2) - rect.right / 2, (ScreenHeight / 2) - rect.bottom / 2, &rect);
 	return true;
 }
 
 void CHudInventory::MsgFunc_Inventory(const char* pszName, BufferReader& reader)
 {
-	int x = reader.ReadByte();
-	m_fOn = 0 != x;
-	g_iVisibleMouse = m_fOn;
+	if (!m_fOn && !(!iMouseInUse && !gHUD.m_iIntermission && !g_iVisibleMouse))
+	{
+		int x = reader.ReadByte();
+		return;
+	}
+
+	g_iVisibleMouse = m_fOn = (0 != reader.ReadByte());
 
 	if (g_iVisibleMouse)
 	{
